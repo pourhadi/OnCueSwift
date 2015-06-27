8d405f0fe28345acaf7fd81beaa1d3d89b913e2f
Daniel Pourhadi <dan.p@akta.com>
2015-06-27 04:06:03 -0500

    Build on 2015-06-27 at 04:06:03 AM

--- OnCueX/ViewControllers/ListLayout.swift
+++ OnCueX/ViewControllers/ListLayout.swift
@@ -61,7 +61,7 @@ class ListLayout: UICollectionViewFlowLayout {
         super.prepareLayout()
         
         self.attributes.removeAll()
-        let offset = self.collectionView!.contentOffset.y
+        let offset = self.collectionView!.contentOffset.y  - self.collectionView!.contentInset.top
         
         let numOfSections = self.collectionView!.numberOfSections()
         for x in 0..<numOfSections {
@@ -74,8 +74,9 @@ class ListLayout: UICollectionViewFlowLayout {
                 
                 let topArea = offset + height
                 if attr.frame.origin.y > offset && attr.frame.origin.y < topArea {
-                    let percent = CalculatePercentComplete(offset, end: topArea, current: attr.frame.origin.y)
+                    let percent = CalculatePercentComplete(topArea, end: offset, current: attr.frame.origin.y)
                     attr.transform3D = MakeUpSwing(percent)
+                    
                 }
                 section.append(attr)
 
