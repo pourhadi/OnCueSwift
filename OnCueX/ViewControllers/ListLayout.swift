94b770a35a37989f9a467366dedbbafb43c4d6e8
Daniel Pourhadi <dan.p@akta.com>
2015-06-27 04:36:00 -0500

    Build on 2015-06-27 at 04:36:00 AM

--- OnCueX/ViewControllers/ListLayout.swift
+++ OnCueX/ViewControllers/ListLayout.swift
@@ -92,7 +92,7 @@ class ListLayout: UICollectionViewFlowLayout {
                 if attr.frame.origin.y > topArea && attr.frame.origin.y < offset {
                     let percent = CalculatePercentComplete(offset, end: topArea, current: attr.frame.origin.y)
                     attr.transform3D = MakeUpSwing(percent)
-                    attr.alpha = percent
+                    attr.alpha = ExtrapolateValue(1, 0, percent)
                     print(percent)
                     
                 }
