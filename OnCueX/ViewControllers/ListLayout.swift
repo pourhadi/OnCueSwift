0cac8ea04f00c5fd176de1647311ccf521641af6
Daniel Pourhadi <dan.p@akta.com>
2015-06-27 03:52:48 -0500

    Build on 2015-06-27 at 03:52:48 AM

--- OnCueX/ViewControllers/ListLayout.swift
+++ OnCueX/ViewControllers/ListLayout.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
+func ExtrapolateValue(from:CGFloat, _ to:CGFloat, _ percent:CGFloat) -> CGFloat {
+    let value = from + ((to - from) * percent)
+    return value
+}
+
+func CATransform3DMake(m11:CGFloat, _ m12:CGFloat, _ m13:CGFloat, _ m14:CGFloat,
+    _ m21:CGFloat, _ m22:CGFloat, _ m23:CGFloat, _ m24:CGFloat,
+    _ m31:CGFloat, _ m32:CGFloat, _ m33:CGFloat, _ m34:CGFloat,
+    _ m41:CGFloat, _ m42:CGFloat, _ m43:CGFloat, _ m44:CGFloat) -> CATransform3D
+{
+    var t = CATransform3D();
+    t.m11 = m11; t.m12 = m12; t.m13 = m13; t.m14 = m14;
+    t.m21 = m21; t.m22 = m22; t.m23 = m23; t.m24 = m24;
+    t.m31 = m31; t.m32 = m32; t.m33 = m33; t.m34 = m34;
+    t.m41 = m41; t.m42 = m42; t.m43 = m43; t.m44 = m44;
+    return t
+}
+
+func MakeUpSwing(percent:CGFloat) -> CATransform3D {
+    let m22End:CGFloat = 0;
+    let m22Start:CGFloat = 1.0;
+    let m24End:CGFloat = -0.0010;
+    let m24Start:CGFloat = 0;
+    let m32End:CGFloat = 1;
+    let m32Start:CGFloat = 0;
+    let m44End:CGFloat = 0.9;
+    let m44Start:CGFloat = 1.0;
+    let m22:CGFloat = ExtrapolateValue(m22Start, m22End, percent);
+    let m24:CGFloat = ExtrapolateValue(m24Start, m24End, percent);
+    let m32:CGFloat = ExtrapolateValue(m32Start, m32End, percent);
+    let m44:CGFloat = ExtrapolateValue(m44Start, m44End, percent);
+
+    let transform = CATransform3DMake(
+        1, 0, 0, 0,
+        0, m22, 0, m24,
+        0, m32, 1, 0,
+        0, 0, 0, m44)
+    return transform;
+}
+
 class ListLayout: UICollectionViewFlowLayout {
 
     var attributes:[UICollectionViewLayoutAttributes] = []
