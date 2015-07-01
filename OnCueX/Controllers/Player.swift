acc16cff91afd4cfb3dc4a0048df047c7daa290e
Daniel Pourhadi <dan.p@akta.com>
2015-07-01 12:39:17 -0500

    Build on 2015-07-01 at 12:39:17 PM

---
+++ OnCueX/Controllers/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  OnCueX
+//
+//  Created by Daniel Pourhadi on 6/30/15.
+//  Copyright © 2015 Daniel Pourhadi. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+
+}
---
+++ OnCueX/Models/Audio.swift
@@ -0,0 +1,13 @@
+//
+//  Audio.swift
+//  OnCueX
+//
+//  Created by Daniel Pourhadi on 6/30/15.
+//  Copyright © 2015 Daniel Pourhadi. All rights reserved.
+//
+
+import UIKit
+
+class AudioBuffer {
+    var ready:Bool = false
+}
